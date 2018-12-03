8324ab465422f96a4655fe7dcc5e1d206f756ad6
Matthias Eiletz <maei@matthiass-mbp.speedport_w_723v_1_45_000>
2018-12-03 19:35:38 +0100

    final

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from flask_jwt import JWT
-from security import authenticate, identity
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogout
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+
+from flask import jsonify
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 
@@ -16,6 +18,11 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 ### ACHTUNG
 app.secret_key = 'matthias' # Das sollte nich öffentlich einsehbar sein
+#app.config['JWT_SECRET_KEY'] = 'matthias'
+
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
 api = Api(app)
 
 @app.before_first_request
@@ -23,7 +30,56 @@ def create_tables():
     db.create_all()
 
 ### ACHTUNG
-jwt = JWT(app, authenticate, identity) #/auth
+jwt = JWTManager(app) # not creating auth endpoint
+###
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1: # should be placed in config file or database
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_token_is_in_checklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired',
+        'error': 'token_expired'
+    }), 401
+
+# when the token in the header send to us, is no JWT token
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+        return jsonify({
+            'description': 'Signature verification failed.',
+            'error': 'Invalid token.'
+        }), 401
+
+# When they dont us a empty header, no JWT token was send
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+        return jsonify({
+            'description': 'Request does not contain an access token.',
+            'error': 'authorization_required.'
+        }), 401
+
+# Demand for a new fresh token, but it wasnt send to us
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+        return jsonify({
+            'description': 'The token is not fresh.',
+            'error': 'fresh_token_required.'
+        }), 401
+
+# The token is no longer available --> like a bann/logout a user --> revoked token list
+@jwt.revoked_token_loader
+def revoked_token_callback():
+        return jsonify({
+            'description': 'The token has been removed.',
+            'error': 'token_revoked.'
+        }), 401
 
 api.add_resource(ItemList, '/items')
 api.add_resource(Item, '/item/<string:name>')
@@ -31,6 +87,9 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
 
 #### IMPORT VON DB
 if __name__ == '__main__':
--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import (jwt_required, get_jwt_claims,
+                                jwt_optional, get_jwt_identity, fresh_jwt_required)
 from models.item import ItemModel
 
 class Item(Resource):
@@ -8,7 +9,7 @@ class Item(Resource):
 
     parser.add_argument('store_id', type=int, required=True, help="Every Item needs a store id")
 
-    #@jwt_required()
+    #@jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
 
@@ -16,7 +17,7 @@ class Item(Resource):
             return item.json()
         return {'message': 'item not found in DB'}, 404
 
-    #@jwt_required()
+    #@jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': 'item already exists in DB'}, 400
@@ -32,10 +33,13 @@ class Item(Resource):
 
         return item.json(), 201
 
-    #@jwt_required()
+    @fresh_jwt_required
     def delete(self, name):
-        deleted_item = ItemModel.find_by_name(name)
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {'message': 'Admin privilege required'}, 401
 
+        deleted_item = ItemModel.find_by_name(name)
         if deleted_item is None:
             return {'message': 'Item "{}" not exist in DB'.format(name)}, 404
 
@@ -43,7 +47,7 @@ class Item(Resource):
 
         return {'message': 'item deleted'}, 201
 
-    #@jwt_required()
+    @jwt_required
     def put(self, name):
         data = Item.parser.parse_args()
 
@@ -60,6 +64,13 @@ class Item(Resource):
         return item.json()
 
 class ItemList(Resource):
-    #@jwt_required()
+    @jwt_optional
     def get(self):
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+
+        return {'items': [item['name'] for item in items],
+                'message': 'More data provided it you are logged in!!'
+        }
